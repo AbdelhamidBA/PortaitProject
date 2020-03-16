2fa59f72641aa34a7bf127cafcff77608c955489
AbdelhamidBA <benabdelfettah.abdelhamid@gmail.com>
2020-03-16 16:54:02 +0100

    Commit

--- routes/film_routes.js
+++ routes/film_routes.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const FilmController = require('../controllers/FilmController');
 router.get('/', FilmController.getFilms);
+router.get('/Count', FilmController.countFilms);
 
 module.exports = router;
